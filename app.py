eb2eeb57ee27becc73e810cd46ae6b8202e38c2f
Mallowmiw <33721791+Mallowmiw@users.noreply.github.com>
2017-11-22 09:58:29 +0700

    test

--- app.py
+++ app.py
@@ -88,7 +88,7 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_text_message(event):
     text = event.message.text
-	
+	  line_bot_api.push_message('Ue0ef5aaa0bb984e7310fac545c068250', TextSendMessage(text='Hello World!'))
     if text == 'profile':
         if isinstance(event.source, SourceUser):
             profile = line_bot_api.get_profile(event.source.user_id)
